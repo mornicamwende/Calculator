9c7bcdcb1241a4b425257468e8044d61d4315aff
mornicamwende <mornicamwende@gmail.com>
2020-07-16 14:30:42 +0300

    add css, index.html, js, styles.css, script.js

---
+++ calculator/js/script.js
@@ -0,0 +1,16 @@
+var add =  function (number1, number2){
+    return number1+number2;
+    };
+    alert(add(10,5));
+    var number1= parseInt(prompt("enter a number"));
+    var number2= parseInt(prompt("enter a number"));
+    
+    alert (add(number1,number2));
+    
+    var subtract=function(number1, number2){
+    return number1-number2
+    };
+    alert(subtract(10,5));
+    var number1= prompt("enter a number");
+    var number2= prompt("enter a number");
+    alert(subtract(number1,number2));
